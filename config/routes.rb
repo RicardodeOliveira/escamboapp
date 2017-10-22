2826b934c32fe9f3a50752d7655a200e6a7d0fc2
Ricardo <Ricardofbdeoliveira@live.com>
2017-10-22 14:38:11 -0200

    Aula 27

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
     resources :categories, except: [:show, :destroy]
     get 'dashboard', to: 'dashboard#index'
+    get 'admins/index'
   end
 
   namespace :site do
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :admins
+  devise_for :admins, :skip => [:registrations]
   devise_for :members
   root 'site/home#index'
 
