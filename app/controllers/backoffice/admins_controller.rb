74a279b2c19bd40a8b77b8145693b37e93352187
Ricardo de Oliveira <Ricardofbdeoliveira@live.com>
2017-10-25 09:49:35 -0200

    Aula 39 COMPLETA

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -3,11 +3,12 @@ class Backoffice::AdminsController < BackofficeController
 
   def index
     #@admins = Admin.all
-    @admins = Admin.with_full_access
+    @admins = Admin.with_restricted_access
   end
 
   def new
     @admin = Admin.new
+    authorize @admin
   end
 
   def create
